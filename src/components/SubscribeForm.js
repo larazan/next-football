4f724f9b85d0986d730e26860ec05c67438ad76f
zamroni <forheron@gmail.com>
2023-07-13 00:22:53 +0700

    add footer

---
+++ src/components/SubscribeForm.js
@@ -0,0 +1,31 @@
+
+
+export default function SubsribeForm() {
+    return (
+        <>
+        <div className="h-max flex flex-col space-y-3 px-2 py-0 md:py-4 justify-center2 items-center2 bg-[#f5f7f9]">
+            <div className="flex flex-col px-4 py-6 mx-auto w-11/12 md:w-12/12 space-y-4  bg-[#000e29]">
+            <span className="uppercase text-lg font-semibold text-white">Newsletter</span>
+            <div>
+                <div className="flex ">
+                <input
+		  type="email"
+		  placeholder="Enter your email"
+		  class="px-4 w-80 rounded bg-[#1a273f] shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+	  />
+
+		  <button class="bg-[#dc052d] hover:opacity-80 text-white py-2 px-4 rounded font-semibold ml-4 ">
+		  Subscribe
+		  </button>
+                </div>
+            </div>
+            <div>
+                <p className="text-xs leading-tight text-white">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                </p>
+            </div>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import HomeAwards from '@/components/HomeAwards'
 import HomeMatch from '@/components/HomeMatch'
+import SubsribeForm from '@/components/SubscribeForm'
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
@@ -18,6 +19,7 @@ export default function Home() {
       <Header />
       <HomeMatch />
       <Banner />
+      <SubsribeForm />
       <HomeAwards />
       <Footer />
     </div>
